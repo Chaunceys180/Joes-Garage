1d6905f3e4c65c1402fa21523aa179341db91369
Chauncey <CBrown-Castro@mail.greenriver.edu>
2019-09-24 20:22:14 -0700

    Added custom bootsrap styling and layout for routing.

---
+++ Controllers/CoffeeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JoesGarage.Controllers
+{
+    public class CoffeeController : Controller
+    {
+        // GET: Coffee
+        public ActionResult Random()
+        {
+            var coffee = new Models.Coffee() { Name = "Houndurian Blend" };
+            
+            return View(coffee);
+        }
+    }
+}
---
+++ Models/Coffee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JoesGarage.Models
+{
+    public class Coffee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        // route will probably be /coffee/random so i need a controller
+    }
+}
